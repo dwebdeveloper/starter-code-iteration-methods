9e0d424987178732bf576038e2df4969c5bae9cf
James Farfan <jamesrojas710@gmail.com>
2020-07-08 22:31:11 -0400

    files update

--- main.js
+++ main.js
@@ -7,3 +7,57 @@ console.log(users)
 
 // Append the katas to this element:
 const main = document.querySelector('main')
+
+function printKata(kataNumber, object) {
+    // For the usage of the DETAILS and SUMMARY tags
+    // in HTML, see: http://mdn.io/details-element
+    const detailsElement = document.createElement('details')
+    main.append(detailsElement)
+    //
+    const summaryElement = document.createElement('summary')
+    summaryElement.append("KATA " + kataNumber)
+    detailsElement.append(summaryElement)
+    //
+    // http://mdn.io/json.stringify
+    const stringifiedObject = JSON.stringify(object)
+    detailsElement.append(stringifiedObject)
+}
+
+const greenEyes1 = users.filter(user => user.eyeColor === "green")
+// OR...
+const greenEyes2 = users.filter(function (user) {
+    return user.eyeColor === "green"
+})
+printKata(0, greenEyes2)
+
+const isTrue = users.filter(function (user) {//01
+    return user.isActive === true
+})
+printKata(1, isTrue)
+
+const emails = users.map(function (user) {//02
+    return user.email
+})
+printKata(2, emails)
+
+const companys = users.some(function (user) {//03
+    if (user.company === "OVATION") {
+        return user.company
+    }
+})
+printKata(3, companys)
+
+const ages = users.find(function (user) {//04
+    if (user.age > 38) {
+        return user.age
+    }
+})
+printKata(4, ages)
+
+//Find the first user in the array over the age of 38 who is active  05
+const activeOne = ages && isTrue
+
+
+
+
+printKata(5, activeOne)
